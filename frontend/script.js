416dc65e9cab4c606d3cf69c9be4cd0b4c3cc1c2
strangeforloop <agohaa1@unlv.nevada.edu>
2020-05-05 23:33:33 -0700

    add tab functionality for lorem type

--- frontend/script.js
+++ frontend/script.js
@@ -1,24 +1,37 @@
 const generateTextButton = document.querySelector('.generate-ipsum');
-let generatedIpsum;
+const tabs = document.querySelectorAll('[data-tab-target]');
+const tabContents = document.querySelectorAll('[data-tab-content]');
 
-const getResults = async function() {
-  const response = await fetch('http://localhost:5000/api/quotes/', {
-    method: 'GET'
+tabs.forEach(tab => {
+  tab.addEventListener('click', () => {
+    const target = document.querySelector(tab.dataset.tabTarget);
+    tabContents.forEach(tabContent => {
+      tabContent.classList.remove('active');
+    });
+    target.classList.add('active');
   });
-  console.log(response);
-  const data = await response.json();
-  console.log(data);
-  let sampleOutput = '';
+});
 
-  for (let i = 1; i < 15; i++) {
-    let randomIndex = Math.floor((Math.random() * (500 - 0)) + 0);
-    sampleOutput += data[randomIndex].quoteTextOnly + ' ';
-  }
 
-  console.log(sampleOutput);
-}
+// const getResults = async function() {
+//   const response = await fetch('http://localhost:5000/api/quotes/', {
+//     method: 'GET'
+//   });
+//   console.log(response);
+//   const data = await response.json();
+//   console.log(data);
+//   let sampleOutput = '';
 
-// getResults();
+//   for (let i = 1; i < 15; i++) {
+//     let randomIndex = Math.floor((Math.random() * (500 - 0)) + 0);
+//     sampleOutput += data[randomIndex].quoteTextOnly + ' ';
+//   }
+
+//   console.log(sampleOutput);
+// }
+
+// // getResults();
+
 const getRadioValue = () => {
   const loremRadioButtons = document.getElementsByName('lorem-type');
 
@@ -32,19 +45,25 @@ const getRadioValue = () => {
 generateTextButton.addEventListener('click', async () => {
   const loremTypeToGenerate = getRadioValue();
   const numberToGenerate = document.getElementById('number-ipsum').value;
+  // move this into just the call for paragraphs
   const removeProfanity = document.getElementById('remove-profanity').checked;
+  // const startWithLatin = document.getElementById('include-latin').checked;
   const outputBox = document.getElementById('output-ipsum');
 
   const response = await fetch(`http://localhost:5000/${loremTypeToGenerate}/${numberToGenerate}/?profanity=${removeProfanity}`);
   const data = await response.json();
   console.log(data);
 
-  let output;
+  let output = '';
+
+  // if (startWithLatin) {
+  //   output += "Twin peaks ipsum dolor sit amet ";
+  // }
 
   if (loremTypeToGenerate === 'paragraphs') {
-    output = generateParagraphOutput(data);
+    output += generateParagraphOutput(data);
   } else if (loremTypeToGenerate === 'words') {
-    output = generateWordOutput(data);
+    output += generateWordOutput(data);
   }
 
   outputBox.innerText = output;
