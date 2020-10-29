92a49f78bfb9fb070c999ca7dd9c62a79a780e4c
strangeforloop <annaagoha@gmail.com>
2020-10-29 10:54:19 -0700

    build

---
+++ build/precache-manifest.a5ef08053267efef47397c6574362482.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a356fae534c5bb274bd540af721c7cb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c5cfe419669a31d53241",
+    "url": "/static/css/main.c8d9d5d7.chunk.css"
+  },
+  {
+    "revision": "17088d38c361749c6294",
+    "url": "/static/js/2.a357b6be.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a357b6be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5cfe419669a31d53241",
+    "url": "/static/js/main.072951c8.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  }
+]);
